49ea913e154d4bfe70c0c22782270108be78558b
Karolis Narkevicius <karolis.n@gmail.com>
2019-02-01 19:47:29 +0000

    Should work!?

---
+++ index.js
@@ -0,0 +1,23 @@
+const standard = require('eslint-plugin-standard')
+const react = require('eslint-plugin-react')
+const promise = require('eslint-plugin-promise')
+const imports = require('eslint-plugin-import')
+const node = require('eslint-plugin-node')
+
+const merged = {
+  rules: {},
+  rulesConfig: {}
+}
+
+merge(standard)
+merge(node)
+merge(promise)
+merge(imports)
+merge(react)
+
+function merge(config) {
+  Object.assign(merged.rules, config.rules)
+  Object.assign(merged.rulesConfig, config.rulesConfig)
+}
+
+module.exports = merged
